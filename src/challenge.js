51af6b88a192b48e812db53076d18a04ab264fc1
Lehlogonolo Mawasha <136694306+LKode12@users.noreply.github.com>
2024-02-06 13:52:50 +0200

    Create challenge.js

---
+++ src/challenge.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, useLocation } from "react-router-dom";
+
+
+export default function Root() {
+    return (<Router> <QueryParamsDemo></QueryParamsDemo></Router>);
+}
+
+function useQuery(){
+    const { search } = useLocation();
+    return React.useMemo(() => new URLSearchParams(search), [search]);
+
+
+}
+
+function QueryParamsDemo(){
+    let query = useQuery();
+    return (
+        <div>
+            <h2>Return Home</h2>
+            <a href={query.get("redirect")}>Click to go home</a>
+        </div>
+    );
+}
